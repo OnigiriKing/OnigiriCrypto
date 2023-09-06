f392245be38c506cf36a38805c9d2edb677c84aa
OnigiriKing <oooNikita87@gmail.com>
2023-09-06 20:19:02 +0700

    Header and style update

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -6,7 +6,19 @@ export default function Header() {
 
   return (
     <header className="header-page">
-
+      <div className="header-wrapper">
+        <div className="header-name">OnigiriCrypto</div>
+        <div className="header-links">
+          <a>Home</a>
+          <a>Market</a>
+          <a>Choose Us</a>
+          <a>Join</a>
+        </div>
+        <div className="header-social">
+          <a>Social</a>
+          <a>Social</a>
+        </div>
+      </div>
     </header>
   );
 }
