0c351ed9ada18aa379e1776500049d61da031822
OnigiriKing <oooNikita87@gmail.com>
2023-09-09 19:00:40 +0700

    JoinUs page jsx created

---
+++ src/Pages/Body/Components/JoinUs.jsx
@@ -0,0 +1,13 @@
+
+export default function Join() {
+
+   return (
+     <div id="join-us-page">
+       <div className="wrapper">
+         <h3>JOIN US VIA DISCORD</h3>
+         <h4>Invest and manage all your crypto at one place.</h4>
+         <button>Join Via discord</button>
+       </div>
+     </div>
+   );
+}
