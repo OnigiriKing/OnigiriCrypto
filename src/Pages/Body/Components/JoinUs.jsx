8e058c6c1efff33760822246eea011fe3bc623bb
OnigiriKing <oooNikita87@gmail.com>
2023-09-10 18:43:19 +0700

    Small updates

--- src/Pages/Body/Components/JoinUs.jsx
+++ src/Pages/Body/Components/JoinUs.jsx
@@ -2,9 +2,11 @@
 export default function Join() {
 
    return (
-     <div id="join-us-page">
-       <div className="wrapper">
-         <h3>JOIN US VIA DISCORD</h3>
+     <div id="join-us-screen">
+       <div className="wrapper join-us-wrapper">
+         <h2>
+           JOIN US VIA <b>DISCORD</b>
+         </h2>
          <h4>Invest and manage all your crypto at one place.</h4>
          <button>Join Via discord</button>
        </div>
--- src/Pages/Body/HomePage.jsx
+++ src/Pages/Body/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Welcome from "./Components/welcomeScreen";
+import Join from "./Components/JoinUs";
 
 export default function HomePage() {
   return (
     <div className="body-page">
     <Welcome />
+    <Join />
     </div>
   );
 }
