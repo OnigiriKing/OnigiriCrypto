1c93705fb084d98b10a046cb4d9227ae0c0e3e4a
OnigiriKing <oooNikita87@gmail.com>
2023-09-26 20:21:52 +0700

    ChooseUs layout update

--- src/components/HomeComp/HomeComp.ChooseUs/ChooseUs.jsx
+++ src/components/HomeComp/HomeComp.ChooseUs/ChooseUs.jsx
@@ -1,25 +1,60 @@
 export default function ChooseUs() {
-
-   return (
-     <div id="choose-us-screen">
-       <div className="wrapper choose-us-wrapper">
-         <h2>
-           WHY <b>CHOOSE US</b>
-         </h2>
-         <div>
-           <div>
-             <div></div>
-             <div></div>
-             <div></div>
-           </div>
-           <div></div>
-           <div>
-             <div></div>
-             <div></div>
-             <div></div>
-           </div>
-         </div>
-       </div>
-     </div>
-   );
+  return (
+    <div id="choose-us-screen">
+      <div className="wrapper choose-us-wrapper">
+        <h2 className="choose-us-baner">
+          WHY <b>CHOOSE US</b>
+        </h2>
+        <div>
+          <div className="choose-us-reasons">
+            <div className="choose-reason">
+              <div className="choose-reason-svg"></div>
+              <div className="choose-reason-text">
+                <h3></h3>
+                <p></p>
+              </div>
+            </div>
+            <div className="choose-reason">
+              <div className="choose-reason-svg"></div>
+              <div className="choose-reason-text">
+                <h3></h3>
+                <p></p>
+              </div>
+            </div>
+            <div className="choose-reason">
+              <div className="choose-reason-svg"></div>
+              <div className="choose-reason-text">
+                <h3></h3>
+                <p></p>
+              </div>
+            </div>
+          </div>
+          <div className="choose-us-img"></div>
+          <div className="choose-us-reasons">
+            <div className="choose-reason">
+              <div className="choose-reason-svg"></div>
+              <div className="choose-reason-text">
+                <h3></h3>
+                <p></p>
+              </div>
+            </div>
+            <div className="choose-reason">
+              <div className="choose-reason-svg"></div>
+              <div className="choose-reason-text">
+                <h3></h3>
+                <p></p>
+              </div>
+            </div>
+            <div className="choose-reason">
+              <div className="choose-reason-svg"></div>
+              <div className="choose-reason-text">
+                <h3></h3>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
