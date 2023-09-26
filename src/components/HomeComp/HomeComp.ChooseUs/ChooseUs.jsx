59ba193424f303df161c203c740901b38db5f308
OnigiriKing <oooNikita87@gmail.com>
2023-09-26 20:14:37 +0700

    Choose us jsx layout ready

---
+++ src/components/HomeComp/HomeComp.ChooseUs/ChooseUs.jsx
@@ -0,0 +1,25 @@
+export default function ChooseUs() {
+
+   return (
+     <div id="choose-us-screen">
+       <div className="wrapper choose-us-wrapper">
+         <h2>
+           WHY <b>CHOOSE US</b>
+         </h2>
+         <div>
+           <div>
+             <div></div>
+             <div></div>
+             <div></div>
+           </div>
+           <div></div>
+           <div>
+             <div></div>
+             <div></div>
+             <div></div>
+           </div>
+         </div>
+       </div>
+     </div>
+   );
+}
--- src/components/HomeComp/HomeComp.jsx
+++ src/components/HomeComp/HomeComp.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Welcome from "./HomeComp.Welcome/Welcome";
 import JoinUs from "./HomeComp.JoinUs/JoinUs";
+import ChooseUs from "./HomeComp.ChooseUs/ChooseUs";
 
 export default function HomeComp() {
   return (
     <div className="body-page">
     <Welcome />
+    <ChooseUs />
     <JoinUs />
     </div>
   );
