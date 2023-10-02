88bd76fe4397a55ec1f652177c5a0fc11d6ce3ff
OnigiriKing <oooNikita87@gmail.com>
2023-10-02 16:09:29 +0700

    CoinList update

--- src/components/HomeComp/HomeComp.CoinList/CoinList.jsx
+++ src/components/HomeComp/HomeComp.CoinList/CoinList.jsx
@@ -5,7 +5,21 @@ export default function CoinList() {
        <div className="wrapper coin-list-wrapper">
          <h1>Market update</h1>
          <div className="coin-list-table">
-           <div className="coin-list-table-banner"></div>
+           <div className="coin-list-table-banner">
+             <h3>Coin</h3>
+             <h3>Price</h3>
+             <h3>24h Change</h3>
+             <h3>Market Cap</h3>
+           </div>
+           <div className="coin-info">
+             <div>
+               <img />
+               <h3></h3>
+             </div>
+             <h3></h3>
+             <h3></h3>
+             <h3></h3>
+           </div>
          </div>
          <div className="coin-list-pages"></div>
        </div>
