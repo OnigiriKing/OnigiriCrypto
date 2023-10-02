fed0dc19ba3401e8cd9ab77b1e9546b345acac26
OnigiriKing <oooNikita87@gmail.com>
2023-10-02 15:40:20 +0700

    CoinList jsx base ready

---
+++ src/components/HomeComp/HomeComp.CoinList/CoinList.jsx
@@ -0,0 +1,14 @@
+export default function CoinList() {
+
+   return (
+     <div id="coin-list-page">
+       <div className="wrapper coin-list-wrapper">
+         <h1>Market update</h1>
+         <div className="coin-list-table">
+           <div className="coin-list-table-banner"></div>
+         </div>
+         <div className="coin-list-pages"></div>
+       </div>
+     </div>
+   );
+}
