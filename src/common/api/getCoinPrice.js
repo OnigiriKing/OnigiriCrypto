dcf560c17706b95165283f06b1682bdb8c2df508
OnigiriKing <oooNikita87@gmail.com>
2023-10-10 19:10:14 +0700

    small update

---
+++ src/common/api/getCoinPrice.js
@@ -0,0 +1,6 @@
+const options = { method: "GET", headers: { accept: "application/json" } };
+
+fetch("https://api-pub.bitfinex.com/v2/ticker/tBTCUSD", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
