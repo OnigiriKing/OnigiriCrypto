1f8132997e7c8d8847e318f83fdc08b5bc00f09b
OnigiriKing <oooNikita87@gmail.com>
2023-10-15 15:42:32 +0700

    Coin component update

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -1,9 +1,26 @@
+import { useParams } from "react-router-dom";
+import CoinList from "components/HomeComp/HomeComp.CoinList/CoinList";
+
 export default function Coin() {
+
+     const { id } = useParams();
 
    return (
        <div id="coin-page" >
         <div className="wrapper coin-page-wrapper">
-        Coins
+        <div>
+            <img></img>
+            <h3></h3>
+            <h3></h3>
+        </div>
+        <div>
+            <div>
+                <h3></h3>
+                <h3></h3>
+                <h3></h3>
+            </div>
+            <div></div>
+        </div>
         </div>
        </div>
     );
