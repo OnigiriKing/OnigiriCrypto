052ae4477c3f9dec9246062bf88965487ea3c186
OnigiriKing <oooNikita87@gmail.com>
2023-10-17 09:57:51 +0700

    Header links update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import allSvg from "svg/otherSvg";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="header-page">
       <div className="wrapper header-footer-wrapper">
         <div className="header-name">
-          <h2>Onigiri Crypto</h2>
+          <Link to={"/"}>Onigiri Crypto</Link>
         </div>
         <div className="header-links">
-          <a href="#">Home</a>
-          <a href="#">Market</a>
-          <a href="#">Choose Us</a>
-          <a href="#">Join</a>
+          <a href="/#welcome-screen">Home</a>
+          <a href="/#coin-list-page">Market</a>
+          <a href="/#choose-us-screen">Choose Us</a>
+          <a href="/#join-us-screen">Join</a>
         </div>
         <div className="header-social">
           <a href="#">{allSvg(10).twitter}</a>
