13b559b1d40ef666cd34cb36d364d2bc0f1bf6c0
OnigiriKing <oooNikita87@gmail.com>
2023-11-04 20:47:03 +0700

    DropMenu created

---
+++ src/components/Header/Header.DropMenu/DropMenu.jsx
@@ -0,0 +1,19 @@
+export default function DropMenu() {
+
+   return (
+     <div id="drop-menu">
+       <div className="drop-menu-wrapper">
+         <div className="drop-menu-nav-links">
+           <a href="/#welcome-screen">Home</a>
+           <a href="/#coin-list-page">Market</a>
+           <a href="/#choose-us-screen">Choose Us</a>
+           <a href="/#join-us-screen">Join</a>
+         </div>
+         <div className="drop-menu-social">
+           <a href="#">{allSvg(10).twitter}</a>
+           <a href="#">{allSvg(10).youTube}</a>
+         </div>
+       </div>
+     </div>
+   );
+}
