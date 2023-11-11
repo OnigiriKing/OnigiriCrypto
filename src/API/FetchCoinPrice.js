dcbab2a8750041921dbc94da72045407d0986ba3
OnigiriKing <oooNikita87@gmail.com>
2023-11-11 15:04:41 +0700

    Netlify update fetchCoinPrice

--- src/API/FetchCoinPrice.js
+++ src/API/FetchCoinPrice.js
@@ -8,22 +8,23 @@ export default function CoinPrice({ crypto, type=6 }) {
 
   React.useEffect(() => {
     window.localStorage.setItem("TickerPrice", price);
+    
   }, [price]);
 
-  async function fetchData() {
-    try {
-      const response = await axios.get(
-        `http://localhost:3000/api/${crypto}-price`
-      );
-      setPrice(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
 
   // it called first only once because of the [],
   // Then it called every 5 seconds
   React.useEffect(() => {
+      async function fetchData() {
+        try {
+          const response = await axios.get(
+            `http://localhost:3000/api/${crypto}-price`
+          );
+          setPrice(response.data);
+        } catch (error) {
+          console.error(error);
+        }
+      }
     fetchData();
     const intervalId = setInterval(() => {
       fetchData();
@@ -32,7 +33,7 @@ export default function CoinPrice({ crypto, type=6 }) {
     return () => {
       clearInterval(intervalId);
     };
-  }, [fetchData]); 
+  }, [crypto]);
 
   const percent = price[4];
 
